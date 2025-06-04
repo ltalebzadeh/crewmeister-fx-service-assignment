f0193ae21139f176014f8648b53ba323f424bb13
leila <l.talebzadeh93@gmail.com>
2025-06-04 18:45:09 +0330

    feat: update original CurrencyConversionRates

--- src/main/java/com/crewmeister/cmcodingchallenge/currency/CurrencyConversionRates.java
+++ src/main/java/com/crewmeister/cmcodingchallenge/currency/CurrencyConversionRates.java
@@ -1,17 +1,30 @@
 package com.crewmeister.cmcodingchallenge.currency;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
 public class CurrencyConversionRates {
-    private double conversionRate;
+    private BigDecimal originalAmount;
+    private String originalCurrency;
+    private BigDecimal convertedAmount;
+    private String targetCurrency;
+    private BigDecimal exchangeRate;
+    private LocalDate date;
 
     public CurrencyConversionRates(double conversionRate) {
-        this.conversionRate = conversionRate;
+        this.exchangeRate = BigDecimal.valueOf(conversionRate);
     }
 
     public double getConversionRate() {
-        return conversionRate;
+        return exchangeRate != null ? exchangeRate.doubleValue() : 0.0;
     }
 
     public void setConversionRate(double conversionRate) {
-        this.conversionRate = conversionRate;
+        this.exchangeRate = BigDecimal.valueOf(conversionRate);
     }
 }
